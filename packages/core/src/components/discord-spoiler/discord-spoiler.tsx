e09d74bca5bdb596f981905b6d29e5b2515a5ab7
derock <derock@derock.dev>
2022-09-20 14:00:11 -0400

    feat: add state to spoiler

--- packages/core/src/components/discord-spoiler/discord-spoiler.tsx
+++ packages/core/src/components/discord-spoiler/discord-spoiler.tsx
@@ -1,13 +1,20 @@
-import { Component, h, Host } from '@stencil/core';
+import { Component, h, Host, Listen, State } from '@stencil/core';
 
 @Component({
 	tag: 'discord-spoiler',
 	styleUrl: 'discord-spoiler.css'
 })
 export class DiscordSpoiler {
+	@State() private isRevealed = false;
+
+	@Listen('click')
+	public reveal() {
+		this.isRevealed = true;
+	}
+
 	public render() {
 		return (
-			<Host class="discord-spoiler">
+			<Host class={`discord-spoiler${this.isRevealed ? '--revealed' : ''}`}>
 				<slot></slot>
 			</Host>
 		);
